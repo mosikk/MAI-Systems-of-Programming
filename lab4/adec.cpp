6ac9b4e9fdc791ee7a716cfa7aaefcd529267ff9
mosikk <moiseenkov_ilya@mail.ru>
2021-03-23 15:10:36 +0300

    upd lab4

--- lab4/adec.cpp
+++ lab4/adec.cpp
@@ -10,16 +10,16 @@ int main() {
 
     addrange(Adec, 0, '0', '9', 1);
     addstr(Adec, 0, "+-", 5);
-    addrange(Adec, 5, '0', '9', 1);
     addrange(Adec, 1, '0', '9', 1);
     addstr(Adec, 1, "eE", 2);
     addstr(Adec, 1, ".", 6);
-    addrange(Adec, 6, '0', '9', 7);
-    addrange(Adec, 7, '0', '9', 7);
-    addstr(Adec, 7, "eE", 2);
     addstr(Adec, 2, "+-", 3);
     addrange(Adec, 3, '0', '9', 4);
     addrange(Adec, 4, '0', '9', 4);
+    addrange(Adec, 5, '0', '9', 1);
+    addrange(Adec, 6, '0', '9', 7);
+    addstr(Adec, 7, "eE", 2);
+    addrange(Adec, 7, '0', '9', 7);
     Adec.final(4);
     
     cout << "Number of states = " << Adec.size() << "\n";
